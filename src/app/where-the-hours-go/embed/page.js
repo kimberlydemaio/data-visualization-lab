cdcf97b10c3d392feb5afb4919ef69f15ba2a46f
Kimberly DeMaio <hello@kimberlydemaio.com>
2026-09-16 15:50:15 -0400

    Add embeddable visualization route

---
+++ src/app/where-the-hours-go/embed/page.js
@@ -0,0 +1,9 @@
+import TimeUseChart from "../TimeUseChart";
+
+export default function WhereTheHoursGoEmbedPage() {
+  return (
+    <main className="min-h-screen bg-[#F1EFE9] px-4 py-4 text-[#25231F]">
+      <TimeUseChart />
+    </main>
+  );
+}
